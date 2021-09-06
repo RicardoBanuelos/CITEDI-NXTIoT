38861d671bdf429c2bc2e555daff68261c905d21
RicardoBanuelos <ricardo.banuelos@uabc.edu.mx>
2021-09-06 12:31:30 -0700

    First Commit

---
+++ first_test/first_test.ino
@@ -0,0 +1,12 @@
+#include <NXTIoT_dev.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  initpayload();
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
